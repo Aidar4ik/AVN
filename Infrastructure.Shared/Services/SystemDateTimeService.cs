365b2dca82882401d3ec970e7ad4233d433512a0
miristan gadaibaev <miristan.kg@gmail.com>
2023-09-20 21:23:22 +0600

    +

---
+++ Infrastructure.Shared/Services/SystemDateTimeService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Interfaces.Shared;
+
+namespace Infrastructure.Shared.Services
+{
+    public class SystemDateTimeService : IDateTimeService
+    {
+        public DateTime NowUtc => DateTime.UtcNow;
+    }
+}
